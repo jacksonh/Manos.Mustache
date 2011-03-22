c4f3f2cc907467235337336e342761b237cf4737
Bojan Rajkovic <brajkovic@coderinserepeat.com>
2011-03-22 13:08:01 -0400

    Add a RenderStache extension method that renders into a string rather than onto a stream.

--- Manos.Mustache.cs
+++ Manos.Mustache.cs
@@ -1,5 +1,3 @@
-
-
 using System;
 using System.IO;
 using Nustache.Core;
@@ -24,6 +22,16 @@ namespace Manos.Mustache {
 			ctx.Response.End ();
 
 	  	}
+
+		public static string RenderStache (this ManosModule mod, string template, object obj) {
+			var t = new Template ();
+			var path = Path.Combine (TEMPLATE_DIR, template);
+			using (var r = new StreamReader (path)) t.Load (r);
+
+			var writer = new StringWriter ();
+			t.Render (obj, writer, locator.GetTemplate);
+			return writer.ToString ();
+		}
 	}
 }
 
